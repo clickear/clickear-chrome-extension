de8fce83c49bdfe51ce3ade50624c003013871a3
chenruochen <chenruochen@qudian.com>
2019-09-19 14:23:38 +0800

    init project

---
+++ README.md
@@ -0,0 +1,25 @@
+# clickear-chrome-extension
+
+> A Vue.js Chrome Extension Project
+
+## [see the refer project](https://github.com/kinglisky/vtemp-chrome-extension)
+
+
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with auto reload && import the **dist** folder in the Chrome browser
+npm run dev
+
+# build for production with minification && crx package
+npm run build
+```
+
+## 
++ Error: read ECONNRESET
+> please see [doc](https://github.com/YuraDev/wcer/pull/6)
+
---
+++ src/popup/App.vue
@@ -0,0 +1,52 @@
+<template>
+    <Tabs value="name1">
+        <TabPane label="标签一" name="name1">标签一1255的内容</TabPane>
+        <TabPane label="标签二" name="name2">标签二的内容</TabPane>
+        <TabPane label="标签三" name="name3">标签三的内容</TabPane>
+    </Tabs>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'This is popup page !'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+html, body {
+  width: 600px;
+  height: 600px;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
